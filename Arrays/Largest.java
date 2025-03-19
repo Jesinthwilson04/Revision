960ccc8a94aa75cd6b8c8528565d43ac241a25b3
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-19 22:17:28 +0530

    largest

--- Arrays/Largest.java
+++ Arrays/Largest.java
@@ -1,5 +1,15 @@
-class Largest{
+import java.util.Arrays;
+import java.util.Collections;
+
+class Largest {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,}
-        int max=arr.MAX_VALUE;
+        int[] arr = { 1, 5, 7, 2, 4, 1 };
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.err.println(max);
+    }
 }
