612a18e47b89c9141e353e6ee364a8dfeca5b621
Jesinthwilson04 <jesinthwilsona.22aid@kongu.edu>
2025-03-20 22:49:37 +0530

    Linear Search and finding the element indice

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,19 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 4, 5, 6, 9 };
+        int find = 4;
+        int findElement = findArr(arr, find);
+        System.out.println(findElement >= 0 ? "The element found at index " + findElement : "Not Found");
+    }
+
+    static int findArr(int[] arr, int find) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == find) {
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+}
